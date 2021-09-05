7c4a70edded2e61b4fe742219ef38e48a57eda10
Oscar9023 <hayate9023@gmail.com>
2021-09-05 18:45:38 +0900

    Add route

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
